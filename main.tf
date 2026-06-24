959c25b6ace9334322a9ec32ab46b13de259348c
Ale <hasbringer1007@gmail.com>
2026-06-24 15:51:27 +0200

    fix(terraform): correct S3 backend config for Garage compat

--- main.tf
+++ main.tf
@@ -10,13 +10,9 @@ terraform {
     bucket = "homelab-terraform-state"
     key    = "terraform.tfstate"
     region = "garage"
-    endpoints = {
-      s3 = "http://192.168.1.142:3900"
-    }
+    endpoint                    = "http://192.168.1.142:3900"
     skip_credentials_validation = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
   }
 }
 
